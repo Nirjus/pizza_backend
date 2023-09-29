df7fb121f95633317f75634161d22f62b5e9cc18
Nirjus Karmakar <128937557+Nirjus@users.noreply.github.com>
2023-09-29 15:45:07 +0530

    bug fix at all orders for admin

--- src/router/orderRoute.js
+++ src/router/orderRoute.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const { isLogIn, isAdmin } = require("../middleware/userAuth");
-const { createOrder, getAllOrders, getSingleOrders, deleteOrder, updateOrder } = require("../controller/orderController");
+const { createOrder, getAllOrders, getSingleOrders,  updateOrder, getAllOrdersForAdmin } = require("../controller/orderController");
 
 
 const orderRoute = express.Router();
 
 orderRoute.post("/create", createOrder);
 orderRoute.get("/", isLogIn,getAllOrders);
-orderRoute.get("/admin",isLogIn,isAdmin, getAllOrders);
+orderRoute.get("/admin",isLogIn,isAdmin, getAllOrdersForAdmin);
 orderRoute.get("/:id", isLogIn,getSingleOrders);
 orderRoute.put("/update/:id",isLogIn,isAdmin, updateOrder);
 
